45334a67d54cd818af3f2c2d43c36c41ed976847
sunglassman <43784491+sunglassman@users.noreply.github.com>
2019-02-04 11:26:25 -0500

    Add files via upload

---
+++ Problem3/problem3.py
@@ -0,0 +1,5 @@
+for x in range (-5, 6):
+	print('x = ' + str(x))
+	y = -2 * x ** 2 + 4 * x - 3
+	print('y = ' + str(y))
+	print()
---
+++ Problem4/problem4.py
@@ -0,0 +1,5 @@
+for t in range(1,51):
+	print ('Year = ' + str(t) + ':')
+	A = 10000 * (2.718) ** (.09 * t)
+	print(A)
+	print()
